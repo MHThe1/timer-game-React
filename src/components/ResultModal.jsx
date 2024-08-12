2770b6e2f89f5290ae39fca1b01eaffede0f0a4e
Mehedi Hasan <mehedihtanvir@gmail.com>
2024-08-12 22:00:42 +0600

    Scoring system added

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,24 +1,44 @@
-import { forwardRef } from "react";
+import { useRef, forwardRef, useImperativeHandle } from "react";
 
-const ResultModal = forwardRef(function ResultModal({ result, targetTime }, ref) {
+const ResultModal = forwardRef(function ResultModal({ targetTime, remainingTime, onReset }, ref) {
+
+    const dialog = useRef();
+
+    const userLost = remainingTime <= 0;
+    const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+    const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
+
+    useImperativeHandle(ref, () => {
+        return {
+            open() {
+                dialog.current.showModal();
+            }
+        }
+    });
+
     return (
-      <dialog
-        ref={ref}
-        className="border-none rounded-lg p-8 bg-[#d7fcf8] animate-[slide-in-from-top_0.35s_ease-out] backdrop:bg-black/90"
-      >
-        <h2 className="font-['Handjet'] m-0 mb-1 text-3xl uppercase">
-          You {result}
-        </h2>
-        <p className="my-2 text-lg">
-          The target time was <strong className="text-[#10655b]">{targetTime} seconds.</strong>
-        </p>
-        <form method="dialog" className="text-right">
-          <button className="mt-4 px-4 py-2 border-none rounded-md bg-[#12352f] text-[#edfcfa] text-lg cursor-pointer hover:bg-[#051715]">
-            Close
-          </button>
-        </form>
-      </dialog>
+        <dialog
+            ref={dialog}
+            className="border-none rounded-lg p-8 bg-[#d7fcf8] animate-[slide-in-from-top_0.35s_ease-out] backdrop:bg-black/90"
+        >
+            <h2 className="font-['Handjet'] m-0 mb-1 text-3xl uppercase">
+                {userLost && "You Lost!"}
+                {!userLost &&
+                    <p>Your Score: <strong className="text-[#ff4242]">{score}</strong></p>}
+            </h2>
+            <p className="my-2 text-lg">
+                The target time was <strong className="text-[#10655b]">{targetTime} seconds.</strong>
+            </p>
+            <p className="my-2 text-lg">
+                Time Remaining: <strong className="text-[#ff4242]">{formattedRemainingTime} seconds.</strong>
+            </p>
+            <form onSubmit={onReset} method="dialog" className="text-right">
+                <button className="mt-4 px-4 py-2 border-none rounded-md bg-[#12352f] text-[#edfcfa] text-lg cursor-pointer hover:bg-[#051715]">
+                    Close
+                </button>
+            </form>
+        </dialog>
     );
-  })
-  
-  export default ResultModal;
+})
+
+export default ResultModal;
--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -5,43 +5,54 @@ export default function TimerChallenge({ title, targetTime }) {
     const timer = useRef();
     const dialog = useRef();
 
-    const [timerStarted, setTimerStarted] = useState(false);
-    const [timerExpired, setTimerExpired] = useState(false);
+    const [timeRemaining, setTimeRemaining] = useState(targetTime * 1000);
+    const timerIsActive = timeRemaining > 0 && timeRemaining < targetTime * 1000;
+    
+    if (timeRemaining <= 0) {
+        clearInterval(timer.current);
+        dialog.current.open();
+    }
 
     function handleStart() {
-        setTimerStarted(true);
-        timer.current = setTimeout(() => {
-            setTimerExpired(true);
-            dialog.current.showModal();
-        }, targetTime * 1000);
+        timer.current = setInterval(() => {
+            setTimeRemaining(prevTimeRemaining => prevTimeRemaining - 10)
+        }, 10);
     }
 
     function handleStop() {
-        clearTimeout(timer.current );
+        clearInterval(timer.current);
+        dialog.current.open();
+    }
+
+    function handleReset() {
+        setTimeRemaining(targetTime * 1000);
     }
 
     let timerAnimation = '';
     let timerText = "Timer inactive";
-    if (timerStarted) {
+    if (timerIsActive) {
         timerAnimation = 'animate-flash';
         timerText = "Time is running...";
     }
 
     return (
         <>
-        <ResultModal ref={dialog} result={"Lost!"} targetTime={targetTime} />
+        <ResultModal 
+            ref={dialog} 
+            remainingTime={timeRemaining} 
+            targetTime={targetTime}
+            onReset={handleReset} />
         <section className="flex flex-col items-center justify-center p-8 my-8 mx-auto bg-[linear-gradient(#4df8df,#4df0f8)] text-[#221c18] shadow-lg shadow-[#23222299] rounded-md">
             <h2 className="text-xl tracking-wide m-0 text-center uppercase text-[#221c18]">
                 {title}
             </h2>
-            {timerExpired && <p>You Lost</p>}
             <p className="border border-[#46cebe] rounded-md p-1 m-2 ">
                 {targetTime} second{targetTime > 1 ? 's' : ''}
             </p>
             <button 
-                onClick={timerStarted ? handleStop : handleStart}
+                onClick={timerIsActive ? handleStop : handleStart}
                 className="mt-4 px-4 py-2 border-none rounded-md bg-[#12352f] text-[#edfcfa] text-lg cursor-pointer hover:bg-[#051715]">
-                {timerStarted ? "End Challenge" : "Start Challenge"}
+                {timerIsActive ? "End Challenge" : "Start Challenge"}
             </button>
             <p className={timerAnimation}>
                 {timerText}
